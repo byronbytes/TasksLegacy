2aea99534475722c893beee9850fc6281d7e978d
byron <53088136+byronbytes@users.noreply.github.com>
2021-07-30 17:46:45 -0400

    Theme but it is broken

--- Tasks/frmSettings.Designer.cs
+++ Tasks/frmSettings.Designer.cs
@@ -76,6 +76,7 @@ namespace Tasks
             this.button1.TabIndex = 4;
             this.button1.Text = "Dark Theme";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
--- Tasks/frmSettings.cs
+++ Tasks/frmSettings.cs
@@ -17,8 +17,14 @@ namespace Tasks
 
         private void button2_Click(object sender, System.EventArgs e)
         {
+            this.BackColor = Color.FromArgb(235, 230, 235);
             frmMain Main = new frmMain(); // Initialize form
             Main.BackColor = Color.FromArgb(235, 230, 235);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            this.BackColor = Color.FromArgb(48, 48, 48);
         }
     }
 }
