be00f5a1cb62d52190764ee756b81ae1b36cb972
byron <53088136+byronbytes@users.noreply.github.com>
2021-07-31 14:33:25 -0400

    Task Manager working (beta)

--- Tasks/frmTaskManager.cs
+++ Tasks/frmTaskManager.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace Tasks
 {
@@ -8,5 +10,39 @@ namespace Tasks
         {
             InitializeComponent();
         }
+
+        Process[] proc;
+
+        void GetAllProcess()
+        {
+            proc = Process.GetProcesses();
+            listBox1.Items.Clear();
+            foreach (Process p in proc)
+                listBox1.Items.Add(p.ProcessName);
+        }
+        private void frmTaskManager_Load(object sender, System.EventArgs e)
+        {
+            GetAllProcess();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                proc[listBox1.SelectedIndex].Kill();
+                GetAllProcess();
+            }
+            catch(Exception ex )
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+  
+        }
+
+ 
     }
 }
